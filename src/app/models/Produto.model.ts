784644761b8dc3b46149b4f88ac21131923c0a24
isaacQ13 <isaac.quinto@ufpe.br>
2023-03-05 17:44:36 -0300

    ultima Atualização

---
+++ src/app/models/Produto.model.ts
@@ -0,0 +1,11 @@
+export class produto{
+    id: number = 0;
+    produto: string = "";
+    descricao: string = "";
+    foto: string = "";
+    preco: number = 0;
+
+    constructor(id: number, pruduto: string, descricao: string, foto: string, preco: number){
+        
+    }
+}
